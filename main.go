09bf7cf4d3c47ea79206bb8f9941bf6a520139c6
richmanfx <richmanfx@yandex.ru>
2020-02-05 07:14:38 +0300

    Установка значения семафора из под пользователя-администратора.

--- main.go
+++ main.go
@@ -7,26 +7,73 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("godror", "system/oracle@localhost:1521/xe")
+
+	newSemaphoreValue := 0
+	userName := "IBS"
+	userPassword := "IBS"
+	standName := "AAAAA"
+	standIP := "11.22.33.44"
+	var semaphoreValue string
+
+	/* Открыть соединение */
+	db, err := sql.Open("godror", fmt.Sprintf("%s/%s@%s:1521/%s", userName, userPassword, standIP, standName))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer db.Close()
 
-	rows, err := db.Query("select sysdate from dual")
+	/* Поставить блокировку */
+	query := "select IBS.executor.lock_open() from dual"
+	_, err = db.Query(query)
 	if err != nil {
-		fmt.Println("Error running query")
-		fmt.Println(err)
+		fmt.Printf("Error executing LockOpen query: %s", err)
 		return
 	}
-	defer rows.Close()
 
-	var thedate string
-	for rows.Next() {
+	/* Прочитать значение семафора */
+	semaphoreValue = getSemaphoreValue(err, db)
+	fmt.Printf("Old semaphore value: '%s'\n", semaphoreValue)
 
-		rows.Scan(&thedate)
+	/* Установить значение семафора */
+	query = fmt.Sprintf("update IBS.Z#SHB_DWH_UNLOAD_S set c_status = %d where id = 1261992816", newSemaphoreValue)
+	fmt.Printf("Set semaphore value to: '%d'\n", newSemaphoreValue)
+	_, err = db.Query(query)
+	if err != nil {
+		fmt.Printf("Error setting Semaphore query: %s", err)
+		return
 	}
-	fmt.Printf("The date is: %s\n", thedate)
 
+	/* Прочитать значение семафора */
+	semaphoreValue = getSemaphoreValue(err, db)
+	fmt.Printf("New semaphore value: '%s'\n", semaphoreValue)
+
+	/* Закрыть соединение */
+	err = db.Close()
+	if err != nil {
+		fmt.Printf("Error close DB: %s", err)
+	}
+
+}
+
+/* Вернуть значение семафора */
+func getSemaphoreValue(err error, db *sql.DB) string {
+
+	var semaphoreValue string
+
+	rows, err := db.Query("select c_status from IBS.Z#SHB_DWH_UNLOAD_S where id = 1261992816")
+	if err != nil {
+		fmt.Printf("Error running Get Semaphore query: %s", err)
+	} else {
+		for rows.Next() {
+			err = rows.Scan(&semaphoreValue)
+			if err != nil {
+				fmt.Printf("Error in Scan: %s", err)
+			}
+		}
+		err = rows.Close()
+		if err != nil {
+			fmt.Printf("Error close rows: %s", err)
+		}
+	}
+	return semaphoreValue
 }
